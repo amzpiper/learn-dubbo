63bbd00ff5fd41d492a62d6b4c6ae8ccec57adb6
郭宇航 <1131717341@qq.com>
2021-07-05 17:21:35 +0800

    gmall用户服务提供者

---
+++ dubbo-helloworld/user-service-provider/src/main/java/com/atguigu/gmall/impl/UserServiceImpl.java
@@ -0,0 +1,23 @@
+package com.atguigu.gmall.impl;
+
+import com.atguigu.gmall.bean.UserAddress;
+import com.atguigu.gmall.service.UserService;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+/**
+ * @date 2021/7/5
+ * @author guoyh
+ */
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public List<UserAddress> getUserAddressList(String userId) {
+        UserAddress userAddressOne = new UserAddress();
+        UserAddress userAddressTwo = new UserAddress();
+
+        return Arrays.asList(userAddressOne, userAddressTwo);
+    }
+}
