e3a5ff0a6a51de9636a860d0dff43729504156b0
郭宇航 <1131717341@qq.com>
2021-07-05 17:28:36 +0800

    gmall用户服务消费者

---
+++ dubbo-helloworld/user-service-consumer/src/main/java/com/atguigu/gmall/impl/OrderServiceImpl.java
@@ -0,0 +1,18 @@
+package com.atguigu.gmall.impl;
+
+import com.atguigu.gmall.service.OrderService;
+
+import java.util.Arrays;
+
+
+/**
+ * @date 2021/7/5
+ * @author guoyh
+ */
+public class OrderServiceImpl implements OrderService {
+
+    @Override
+    public void initOrder(String userId) {
+
+    }
+}
---
+++ dubbo-helloworld/user-service-consumer/src/main/java/com/atguigu/gmall/service/OrderService.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.service;
+
+import com.atguigu.gmall.bean.UserAddress;
+import java.util.List;
+
+/**
+ * @date 2021/7/5
+ * @author guoyh
+ */
+public interface OrderService {
+
+    public void initOrder(String userId);
+}
