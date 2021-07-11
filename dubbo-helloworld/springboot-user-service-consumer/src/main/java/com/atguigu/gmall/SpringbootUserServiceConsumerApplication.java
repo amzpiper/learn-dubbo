06341a995bfa0a73756c9851b61bf03eb3649122
郭宇航 <1131717341@qq.com>
2021-07-11 16:27:00 +0800

    搭建springboot-duubo整合

---
+++ dubbo-helloworld/springboot-user-service-consumer/src/main/java/com/atguigu/gmall/SpringbootUserServiceConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUserServiceConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootUserServiceConsumerApplication.class, args);
+    }
+
+}
